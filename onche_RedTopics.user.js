723a5126c1867be48ecc7ed5c8653003f0411e7e
tintinabule <127985974+tintinabule@users.noreply.github.com>
2023-03-15 20:13:31 +0100

    v0.3

--- onche_RedTopics.user.js
+++ onche_RedTopics.user.js
@@ -1,16 +1,33 @@
 // ==UserScript==
 // @name     [ONCHE]_RedTopics
 // @author 	 tintinabule
-// @version  0.2
+// @version  0.3
 // @grant    none
 // @match https://onche.org/forum/1/blabla-general*
 // ==/UserScript==
 
+
+
+///////////////// PARAMS
+
+// Mettre à 0 pour ne pas changer les avatars des topics en une icone de fichier
+let changeImage = 1
+
+
+
+
+
+
+//////////////// HISTORIQUE
 // 0.1 : couleurs du texte
 // 0.2 : ajout icone JVC pour les topics (au lieu des avatars)
+// 0.3 : changeImage pour désactiver les icones de dossier, 
 
 
-console.log("starting");
+
+
+///////////////// CODE, ne pas modifier
+
 
 let topics = document.querySelectorAll(".topic");
 
@@ -19,14 +36,14 @@ topics.forEach(topic => {
   let imgAvatar = topic.querySelector("img");
   let ts = topic.querySelector(".topic-subject"); 
   
-  if (imgAvatar != null) {
+  if (imgAvatar != null && changeImage!=0) {
     imgAvatar.src = "https://raw.githubusercontent.com/tintinabule/JVScript/main/img/rectangle-yellow.png" 
   }
   
   if (parseInt(nb.textContent) > 10) {
     ts.style.color  = "#E99FAA";
     
-    if (imgAvatar != null) {
+    if (imgAvatar != null  && changeImage!=0) {
       imgAvatar.src = "https://raw.githubusercontent.com/tintinabule/JVScript/main/img/rectangle-red.png" 
     }
   } 
