c106da95f38452c966e040d7ee2366e583ebc8a8
tintinabule <127985974+tintinabule@users.noreply.github.com>
2023-03-15 19:58:19 +0100

    Update onche_RedTopics.user.js

--- onche_RedTopics.user.js
+++ onche_RedTopics.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name     [ONCHE]_RedTopics
 // @author 	 tintinabule
-// @version  0.1
+// @version  0.2
 // @grant    none
 // @match https://onche.org/forum/1/blabla-general*
 // ==/UserScript==
 
-
+// 0.1 : couleurs du texte
+// 0.2 : ajout icone JVC pour les topics (au lieu des avatars)
 
 
 console.log("starting");
@@ -14,12 +15,21 @@ console.log("starting");
 let topics = document.querySelectorAll(".topic");
 
 topics.forEach(topic => {
+  let nb = topic.querySelector(".topic-nb");
+  let imgAvatar = topic.querySelector("img");
   let ts = topic.querySelector(".topic-subject"); 
   
+  if (imgAvatar != null) {
+    imgAvatar.src = "https://raw.githubusercontent.com/tintinabule/JVScript/main/img/rectangle-yellow.png" 
+  }
   
   if (parseInt(nb.textContent) > 10) {
     ts.style.color  = "#E99FAA";
-  }
+    
+    if (imgAvatar != null) {
+      imgAvatar.src = "https://raw.githubusercontent.com/tintinabule/JVScript/main/img/rectangle-red.png" 
+    }
+  } 
   
   if (parseInt(nb.textContent) > 100) {
     ts.style.color  = "#e3ae00";
