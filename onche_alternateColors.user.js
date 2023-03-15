5c69aa4a370a3c43aa02be4698de49796a2d6c80
tintinabule <127985974+tintinabule@users.noreply.github.com>
2023-03-16 00:17:18 +0100

    Update onche_alternateColors.user.js

--- onche_alternateColors.user.js
+++ onche_alternateColors.user.js
@@ -1,10 +1,16 @@
 // ==UserScript==
 // @name     [ONCHE]_AlternateColors
 // @author 	 tintinabule
-// @version  0.1
+// @version  0.2
 // @grant    none
 // @match https://onche.org/topic*
 // ==/UserScript==
+
+//////////////// HISTORIQUE
+// 16.03
+// 0.2 : change l'affichage de la date
+// 15.03
+// 0.1 : couleurs de bckgd
 
 let msgs = document.querySelectorAll(".message:not([class*=\" \"])");
 
@@ -14,3 +20,9 @@ for (var i = 0; i < msgs.length; i += 2) {
   msg1 = msgs[i+1];
   msg1.style.background = "#2e3238"; 
 };
+
+let msgdates = document.querySelectorAll(".message-date");
+
+msgdates.forEach(msgdate => {
+  msgdate.innerHTML = msgdate.title;
+});
